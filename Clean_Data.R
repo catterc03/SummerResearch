f35cb9a03caf10dd39bc0439f56d06ac195fe2b6
catterc03 <christophercatterick@gmail.com>
2025-06-23 23:33:20 -0400

    Recommit for Clean_Data

--- Clean_Data.R
+++ Clean_Data.R
@@ -1,3 +1,16 @@
-View(Data)
-summary(Data)
-plot(Data$ym, Data$cases)
+# Name: Christopher Catterick
+# Version: 1.01
+# Desc: Base program to clean data for 2025 Summer Research. This program cleans data 
+# of BC employment assistance cases with respect to CMACAs and also includes number of recips 
+# and depchlds. 
+
+#Assign Data to represent dataset
+Data <- bcea_cmaca 
+
+#Assigning objects for each column
+cmaca <- Data$cmaca 
+cases <- Data$cases
+recips <- Data$recips
+depchld <- Data$depchld
+
+
