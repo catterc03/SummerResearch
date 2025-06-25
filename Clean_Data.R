cd903073c20da358153ea0bfe6623dc6c776254a
catterc03 <christophercatterick@gmail.com>
2025-06-25 00:30:22 -0400

    Commit for updated code, added functionality for data filtering by YM and CMACA

--- Clean_Data.R
+++ Clean_Data.R
@@ -4,13 +4,22 @@
 # of BC employment assistance cases with respect to CMACAs and also includes number of recips 
 # and depchlds. 
 
+library(dplyr)
+
 #Assign Data to represent dataset
 Data <- bcea_cmaca 
 
 #Assigning objects for each column
+ym <- Data$ym
 cmaca <- Data$cmaca 
 cases <- Data$cases
 recips <- Data$recips
 depchld <- Data$depchld
+
+#Method to filter data by date(YYYYMM) and CMACA given by respective dictionary code
+fdata <- Data |>
+  filter(cmaca == "905" , ym >= 200001, ym <= 200012)
+
+View(fdata)
 
 
