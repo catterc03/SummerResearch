686dcad557cfa74407f9626269136c7192663bc8
catterc03 <christophercatterick@gmail.com>
2025-07-03 20:21:19 -0400

    Update functionality to include aggregation of CMA by year for use in further analysis

--- Clean_Data.R
+++ Clean_Data.R
@@ -1,5 +1,5 @@
 # Name: Christopher Catterick
-# Version: 1.02
+# Version: 1.1
 # Desc: Base program to clean data for 2025 Summer Research. This program cleans data 
 # of BC employment assistance cases with respect to CMACAs and also includes number of recips 
 # and depchlds. 
@@ -7,6 +7,7 @@
 library(dplyr)
 
 #Assign Data to represent dataset
+Data <- bcea_cmaca 
 Data <- bcea_cmaca 
 
 #Assigning objects for each column
@@ -16,16 +17,37 @@ cases <- Data$cases
 recips <- Data$recips
 depchld <- Data$depchld
 
+#Mutate Data to create columns for year and CMA in the correct format
+Data <- Data %>%
+  mutate(
+    year = as.integer(substr(as.character(ym),1 ,4 )),
+    cmaca = as.character(cmaca)
+  )
+
+#Create agg_yearly to aggregate the values from raw dataset into a Year based format
+agg_yearly <- Data %>%
+  group_by(cmaca, year) %>%
+  summarize(
+    cases = sum(cases, na.rm = TRUE),
+    recips = sum(recips, na.rm = TRUE),
+    depchld = sum(depchld, na.rm = TRUE),
+    .groups = "drop"
+  )
+
+# Lists all CMA codes present in the data set
+cma_list <- split(agg_yearly, agg_yearly$cmaca)
+
+#Test line which outputs data from 1 CMA "905"
+cma_905 <- cma_list[["905"]]
+
 #Function to filter data by date(YYYYMM) and CMACA given by respective dictionary code,
 # data: takes in data set to filter
 # Area: Takes in string of area code to filter by respective area code
 # Min: Takes the min timeframe (YYYYMM)
 # Max: Take the max timeframe (YYYYMM)
 
-fdata <- function(data, area, min, max) {
-  data |>
-    filter(cmaca == area, ym >= min, ym <= max)
-}
-
-View(fdata(Data, "907", 200001, 200012))
+# fdata <- function(data, area, min, max) {
+#   data |>
+#     filter(cmaca == area, ym >= min, ym <= max)
+# }
 
